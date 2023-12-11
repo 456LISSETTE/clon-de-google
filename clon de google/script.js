25d8e53cb05155088db55c6fdc5fffbbc19b9847
456LISSETTE <150540639+456LISSETTE@users.noreply.github.com>
2023-12-11 08:18:18 -0600

    Add files via upload

---
+++ clon de google/script.js
@@ -0,0 +1,12 @@
+const preferencesEnlace = document.querySelector(".preferences-enlace");
+const preferensList = document.querySelector(".preferens-list");
+
+preferencesEnlace.addEventListener("click", (e) => {
+  e.preventDefault();
+  
+  if (preferensList.style.display === "none" || preferensList.style.display === "") {
+    preferensList.style.display = "block";
+  } else {
+    preferensList.style.display = "none";
+  }
+});
